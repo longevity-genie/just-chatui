f08ff481005c0ec44dee2adfb87bf62ffa09b9e8
Alex-Karmazin <karmazinalex@gmail.com>
2024-06-23 22:46:45 +0300

    Changes to make it work with genetics genie.

--- src/api/ollama.ts
+++ src/api/ollama.ts
@@ -1,28 +1,24 @@
-import { Ollama } from 'ollama/browser'
-import { getSystemPromptAndSuffix } from '../store/SystemPromptController'
+import { OpenAI } from 'openai'
+
 
 export const ollamaApiCall = async (
   content: string,
   callback: (str: string) => void
 ) => {
-  const messages = [
-    { role: 'system', content: getSystemPromptAndSuffix() },
-    { role: 'user', content }
-  ]
 
-  const ollama = new Ollama({ host: 'http://10.0.1.22:11434' })
-  const response = await ollama.chat({
-    model: 'wizardlm2',
-    messages,
+  const openai = new OpenAI({
+    baseURL: 'http://agingkills.eu:8088/v1', // change to http://localhost:8088/v1 for local launch
+    apiKey: 'ollama', // required but unused
+    dangerouslyAllowBrowser: true
+  });
+
+  const response = await openai.chat.completions.create({
+    messages: [{ content, role: 'user'}],
+    model: 'groq/llama3-70b-8192',
     stream: true,
-    options: {
-      num_ctx: 4096,
-      stop: ['User:', 'user:'],
-      temperature: 0.9
-    }
-  })
+  });
 
   for await (const part of response) {
-    callback(part.message.content)
+    callback(part.choices[0]?.delta?.content || '');
   }
 }
